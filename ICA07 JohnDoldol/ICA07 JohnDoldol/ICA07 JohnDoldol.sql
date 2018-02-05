fcc468287ab1410e4da5ffc54d64c1ab2bbcf9ba
John P Doldol <jdoldol1@nait.ca>
2018-02-05 14:30:57 -0700

    stuck on q8

--- ICA07 JohnDoldol/ICA07 JohnDoldol/ICA07 JohnDoldol.sql
+++ ICA07 JohnDoldol/ICA07 JohnDoldol/ICA07 JohnDoldol.sql
@@ -33,6 +33,99 @@ order by [Last Name]
 go
 
 --q3
+select
+	ProductName as 'Product Name',
+	UnitPrice as 'Unit Price'
+from Products
+where SupplierID in
+	(
+		select distinct SupplierID
+		from Suppliers
+		where Country in('sweden','italy')
+	)
+order by UnitPrice
+go	
+
+--q4
+select
+	ProductName as 'Product Name',
+	UnitPrice as 'Unit Price'
+from Products as outy
+where exists
+	(
+		select distinct SupplierID
+		from Suppliers as iny
+		where (outy.SupplierID = iny.SupplierID) and (iny.Country in('sweden','italy'))
+	)
+order by [Unit Price]
+go
+
+--q5
+declare @unitvalue as int = 20
+select ProductName
+from Products as outter
+where CategoryID in
+	(
+		select CategoryID
+		from Categories
+		where CategoryName in('seafood','confections')
+	)
+and UnitPrice > @unitvalue
+order by CategoryID, ProductName
+go
+
+--q6
+declare @uvalue as int = 20
+select 
+	ProductName
+from Products as outy
+where exists
+	(
+		select CategoryID
+		from Categories as inny
+		where (outy.CategoryID = inny.CategoryID) and 
+		(inny.CategoryName in('seafood','confections'))
+	)
+and UnitPrice > @uvalue
+order by CategoryID, ProductName
+go
+
+--q7
+declare @cost as int = 15
+select
+	CompanyName as 'Company Name',
+	Country as 'Country'
+from Customers
+where CustomerID in
+	(
+		select CustomerID
+		from Orders
+		where OrderID in 
+			(
+				select OrderID
+				from [Order Details]
+				where (Quantity * UnitPrice) < @cost
+			)	
+	)
+order by Country
+go
+
+--q8
+declare @procost as int = 15
+select
+	CompanyName as 'Company Name',
+	Country as 'Country'
+from Customers
+
+
+
+	
+
+
+	
+
+
+
 
 
 
