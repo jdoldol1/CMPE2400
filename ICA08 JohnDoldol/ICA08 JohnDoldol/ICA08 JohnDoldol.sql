7870006bbff4f28dd380e97fc78c17cf2d071d72
John P Doldol <jdoldol1@nait.ca>
2018-02-09 10:38:56 -0700

    --q4 done

--- ICA08 JohnDoldol/ICA08 JohnDoldol/ICA08 JohnDoldol.sql
+++ ICA08 JohnDoldol/ICA08 JohnDoldol/ICA08 JohnDoldol.sql
@@ -9,3 +9,31 @@ order by Country
 go
 
 --q2
+select top 1 with ties
+	CompanyName as 'Supplier Company Name',
+	Country as 'Country'
+from Suppliers
+order by Country
+go
+
+--q3
+select top 10 percent
+	ProductName as 'Product Name',
+	UnitsInStock as 'Units in Stock'
+from Products
+order by [Units in Stock] desc
+
+--q4
+select 
+	CompanyName as 'Customer Company Name',
+	Country as 'Country'
+from Customers
+where CustomerID in 
+	(
+		select top 8 CustomerID
+		from Orders
+		order by Freight desc
+	)
+go
+
+--q5
