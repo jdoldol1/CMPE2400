5316d0f303a44865ecf9707d0956d537bec8103e
John P Doldol <jdoldol1@nait.ca>
2018-03-13 09:40:29 -0600

    --q2

--- ICA11 JohnDoldol.sql
+++ ICA11 JohnDoldol.sql
@@ -9,3 +9,14 @@ order by 'Num Orders' desc
 go
 
 --q2
+select 
+	CONCAT(e.LastName, ', ', e.FirstName) as 'Name',
+	AVG(o.Freight) as 'Average Freight',
+	convert(varchar,max(o.OrderDate),106) as 'Newest Order Date'
+from Employees as e inner join Orders as o
+on e.EmployeeID = o.EmployeeID
+group by e.LastName, e.FirstName
+order by max(o.OrderDate), e.LastName
+ go
+
+--q3
