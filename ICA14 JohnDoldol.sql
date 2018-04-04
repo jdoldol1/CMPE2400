8ff8fdaa97c430630e766e43cb2cd0803ea874f8
John Carlo Doldol <31707603+jdoldol1@users.noreply.github.com>
2018-04-04 15:12:23 -0600

    --q2 done

--- ICA14 JohnDoldol.sql
+++ ICA14 JohnDoldol.sql
@@ -50,7 +50,7 @@ go
 create procedure ica14_02
 @year as int,
 @coname as varchar(64) output,
-@average as money
+@average as money output
 as
 	select top 1
 		@coname = CONCAT(e.LastName, ', ', e.FirstName),
@@ -58,8 +58,27 @@ as
 	from NorthwindTraders.dbo.Employees as e
 	inner join NorthwindTraders.dbo.Orders as o
 	on e.EmployeeID = o.EmployeeID	
+	where DATEPART(year, o.OrderDate) = @year
 	group by e.LastName, e.FirstName
-	order by max(o.OrderDate), e.LastName
+	order by AVG(o.Freight) desc
 go
 
 
+declare @YEAR as int = 1996
+declare @fullname as varchar(64)
+declare @freight as money
+
+exec ica14_02 @YEAR,
+	@fullname output, @freight output
+select 
+	@YEAR as 'Year',
+	@fullname as 'Name',
+	@freight as 'Biggest Avg Freight'
+
+set @YEAR = 1997
+exec ica14_02 @year = @YEAR, @coname = @fullname output, @average = @freight output
+
+select 
+	@YEAR as 'Year',
+	@fullname as 'Name',
+	@freight as 'Biggest Avg Freight'
