651f45ff872de344debc401735e9e89aa24e4eb9
John Carlo Doldol <31707603+jdoldol1@users.noreply.github.com>
2018-04-10 09:52:59 -0600

    temp database added --q1A done

---
+++ ICA15 JohnDoldol.sql
@@ -0,0 +1,75 @@
+﻿-- ica15
+-- This ICA is comprised of 2 parts, but should be tackled as described by your instructor.
+-- To ensure end-to-end running, you will have to complete the ica in pairs where possible :
+--  q1A + q2A, then q1B + q2B
+-- You will need to install a personal version of the ClassTrak database
+-- The Full and Refresh scripts are on the Moodle site.
+-- Once installed, you can run the refresh script to restore data that may be modified or 
+--  deleted in the process of completing this ica.
+
+use [jdoldol_ClassTrak] -- YOUR_COPY_CLASSTRAK
+go
+
+-- q1
+-- All in one batch, to retain your variable contexts/values
+
+-- A
+-- Insert a new Instructor : Donald Trump
+--  Check column types, supply necessary values, it may require a column list
+--  Save your identity into a variable
+declare @ins_id as int
+insert into Instructors(first_name,last_name)
+values ('Donald','Trump')
+set @ins_id = @@IDENTITY
+-- B
+-- Insert a new Course : cmpe2442 "Fast and Furious - SQL Edition"
+--  Check column types, supply necessary values, it may require a column list
+--  Save your identity into a variable
+
+-- C
+-- Insert a record indicating your new instructor is teaching the new course
+--  description : "Beware the optimizer"
+--  start_date : use 01 Sep 2016
+--  Save the identity into a variable
+
+-- D Insert a bunch in one insert
+-- Generate the insert statement to Add all the students with a last name that
+--  starts with a vowel to the new class
+
+-- E
+--  Prove it all, generate a select to show :
+--   All instructors - see your new entry
+--   All courses that have SQL in description
+--   All classes that have a start_date after 1 Aug 2016
+--   All students in the new class - filter by description having "Beware"
+--       sort by first name in last name
+
+go
+-- end q1
+
+
+
+-- q2 - Undo all your changes to reset the database, you must do this in reverse order to
+--      ensure you do not attempt to corrupt Referencial Integrity.
+--     As such, work backwards from D to A, deleting what we added, but you must query the DB
+--      to find and save the relevant keys.
+
+-- q2 - Undo all your changes to reset the database, you must do this in reverse order to
+--      ensure you do not attempt to corrupt Referencial Integrity.
+--     As such, work backwards from D to A.
+
+-- D - Delete all students that have been assigned to your new class, do this without a 
+--     variable, rather perform a join with proper filtering for this delete
+
+-- C - declare, query and set class id to your new class based on above filter.
+--     declare, query and save the linked course and instructor ( use in B and A )
+--     Delete the new class
+
+-- B - Delete the new course as saved in C
+
+-- A - Delete the new instructor as saved in C
+
+-- E - Repeat q1 part E to verify the removal of all the data.
+
+
+go
